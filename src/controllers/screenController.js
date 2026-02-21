f088574b0a302a5f14d83f20c57039532ff553d4
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-20 19:30:17 -0800

    Refactor entire code and recreate showForm()

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -1,13 +1,13 @@
 import todoController from "../controllers/todoController.js";
 
 const init = (container) => {
-  const form = container.querySelector(".form");
-  const addBtn = container.querySelector(".addBtn");
+  const addDiv = container.querySelector(".add-div");
+  const addBtn = container.querySelector(".custom-btn.add-todo");
 
   displayFolders();
 
   addBtn.addEventListener("click", () => {
-    showForm(form, addBtn);
+    showForm(addDiv, addBtn);
   });
 };
 
@@ -15,6 +15,7 @@ const displayFolders = () => {
   const archive = todoController.fetchArchive();
   const inbox = document.querySelector(".inbox");
   const projects = document.querySelector(".projects");
+
   inbox.textContent = "";
   projects.textContent = "";
 
@@ -34,12 +35,12 @@ const displayFolders = () => {
 
   archive["projects"].forEach((project) => {
     const headingDiv = document.createElement("div");
+    const nameLine = document.createElement("div");
+    const todosDiv = document.createElement("div");
     const projectName = project.getName();
     const projectTodos = project.getTodos();
 
-    const nameLine = document.createElement("div");
     nameLine.textContent = `Name: ${projectName}`;
-    const todosDiv = document.createElement("div");
     todosDiv.textContent = "Tasks:";
 
     headingDiv.append(nameLine, todosDiv);
@@ -64,56 +65,6 @@ const displayFolders = () => {
   });
 };
 
-const showForm = (container, btn) => {
-  btn.style.display = "none";
-
-  const form = document.createElement("form");
-  const taskLabel = document.createElement("label");
-  const taskInput = document.createElement("input");
-  const addBtn = document.createElement("button");
-
-  taskLabel.textContent = "Task:";
-  taskInput.type = "text";
-  addBtn.type = "submit";
-  addBtn.textContent = "Enter";
-  addBtn.classList.add("add-btn");
-
-  const projectLabel = document.createElement("label");
-  const projectInput = document.createElement("input");
-
-  projectLabel.textContent = "Project:";
-  projectInput.type = "text";
-
-  const taskDiv = document.createElement("div");
-  const projectDiv = document.createElement("div");
-
-  taskDiv.append(taskLabel, taskInput);
-  projectDiv.append(projectLabel, projectInput);
-
-  form.append(taskDiv, projectDiv, addBtn);
-  container.appendChild(form);
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const task = taskInput.value;
-    const project = projectInput.value;
-    if (!task.trim()) return;
-
-    todoController.addTodo(task, project);
-    displayFolders();
-
-    form.remove();
-    btn.style.display = "";
-  });
-};
-
-const randomColor = () => {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-
-  return `rgb(${r}, ${g}, ${b})`;
-};
+const showForm = (container, btn) => {};
 
 export default { init };
