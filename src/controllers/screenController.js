5124df4dc61cf574879ae22eb51e4a9162bfcf23
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-20 16:48:58 -0800

    Lot of things done, time to recharge

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -4,19 +4,21 @@ const init = (container) => {
   const form = container.querySelector(".div");
   const btn = container.querySelector(".addTodo");
 
-  displayFolder("inbox");
+  displayFolders();
 
   btn.addEventListener("click", () => {
-    showForm(form, "inbox", btn);
+    showForm(form, btn);
   });
 };
 
-const displayFolder = (folderName) => {
+const displayFolders = () => {
   const archive = todoController.fetchArchive();
-  const folder = document.querySelector(`.${folderName}`);
-  folder.textContent = "";
+  const inbox = document.querySelector(".inbox");
+  const projects = document.querySelector(".projects");
+  inbox.textContent = "";
+  projects.textContent = "";
 
-  archive[folderName].forEach((todo) => {
+  archive["inbox"].forEach((todo) => {
     const div = document.createElement("div");
     const li = document.createElement("li");
     const deleteBtn = document.createElement("button");
@@ -27,36 +29,91 @@ const displayFolder = (folderName) => {
 
     div.append(li, deleteBtn);
 
-    folder.appendChild(div);
+    inbox.appendChild(div);
+  });
+
+  archive["projects"].forEach((project) => {
+    const headingDiv = document.createElement("div");
+    const projectName = project.getName();
+    const projectTodos = project.getTodos();
+
+    const nameLine = document.createElement("div");
+    nameLine.textContent = `Name: ${projectName}`;
+    const todosDiv = document.createElement("div");
+    todosDiv.textContent = "Tasks:";
+
+    headingDiv.append(nameLine, todosDiv);
+
+    projectTodos.forEach((todo) => {
+      const todoList = document.createElement("div");
+      const li = document.createElement("li");
+      const deleteBtn = document.createElement("button");
+
+      li.textContent = todo.getTitle();
+      deleteBtn.textContent = "Delete";
+      deleteBtn.classList.add("delete-btn");
+
+      todoList.classList.add("custom-div");
+      todoList.append(li, deleteBtn);
+
+      todosDiv.appendChild(todoList);
+
+      headingDiv.appendChild(todosDiv);
+    });
+    projects.append(headingDiv);
   });
 };
 
-const showForm = (container, folder, btn) => {
+const showForm = (container, btn) => {
   btn.style.display = "none";
 
   const form = document.createElement("form");
-  const input = document.createElement("input");
-  const button = document.createElement("button");
+  const taskLabel = document.createElement("label");
+  const taskInput = document.createElement("input");
+  const addBtn = document.createElement("button");
 
-  input.type = "text";
-  button.type = "submit";
-  button.textContent = "Enter";
+  taskLabel.textContent = "Task:";
+  taskInput.type = "text";
+  addBtn.type = "submit";
+  addBtn.textContent = "Enter";
+  addBtn.classList.add("add-btn");
 
-  form.append(input, button);
+  const projectLabel = document.createElement("label");
+  const projectInput = document.createElement("input");
+
+  projectLabel.textContent = "Project:";
+  projectInput.type = "text";
+
+  const taskDiv = document.createElement("div");
+  const projectDiv = document.createElement("div");
+
+  taskDiv.append(taskLabel, taskInput);
+  projectDiv.append(projectLabel, projectInput);
+
+  form.append(taskDiv, projectDiv, addBtn);
   container.appendChild(form);
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const task = input.value;
+    const task = taskInput.value;
+    const project = projectInput.value;
     if (!task.trim()) return;
 
-    todoController.addTodo(task);
-    displayFolder(folder);
+    todoController.addTodo(task, project);
+    displayFolders();
 
     form.remove();
     btn.style.display = "";
   });
+};
+
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
 export default { init };
