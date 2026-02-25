8977ea7b03484357dcdcdc7e22b3339bd4884609
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-25 12:37:00 -0800

    Modify index.js and screenController.js for function init()

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import "./styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import screenController from "./controllers/screenController.js";
 
-const appContainer = document.querySelector("#wrapper");
-
-screenController.init(appContainer);
+screenController.init();
