26ae7e48eb9b58bc2aaa58ed4a0d0b47679d641b
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-25 18:05:55 -0800

    Modify displayFolders() to displayFolder() and update inbox with list of tasks

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -8,63 +8,15 @@ const init = () => {
   const inbox = document.querySelector(".inbox");
   const projects = document.querySelector(".projects");
 
-  displayFolders(inbox, projects);
+  displayFolder(inbox);
 
   addTodo.addEventListener("click", () => {
     showForm(customDiv, inbox, addTodo);
   });
 };
 
-const displayFolders = (inbox, projects) => {
+const displayFolder = (folder) => {
   const archive = todoController.fetchArchive();
-
-  inbox.textContent = "";
-  projects.textContent = "";
-
-  archive["inbox"].forEach((todo) => {
-    const div = document.createElement("div");
-    const li = document.createElement("li");
-    const deleteBtn = document.createElement("button");
-
-    li.textContent = todo.getTitle();
-    deleteBtn.textContent = "Delete";
-    deleteBtn.classList.add("delete-btn");
-
-    div.append(li, deleteBtn);
-
-    inbox.appendChild(div);
-  });
-
-  archive["projects"].forEach((project) => {
-    const headingDiv = document.createElement("div");
-    const nameLine = document.createElement("div");
-    const todosDiv = document.createElement("div");
-    const projectName = project.getName();
-    const projectTodos = project.getTodos();
-
-    nameLine.textContent = `Name: ${projectName}`;
-    todosDiv.textContent = "Tasks:";
-
-    headingDiv.append(nameLine, todosDiv);
-
-    projectTodos.forEach((todo) => {
-      const todoList = document.createElement("div");
-      const li = document.createElement("li");
-      const deleteBtn = document.createElement("button");
-
-      li.textContent = todo.getTitle();
-      deleteBtn.textContent = "Delete";
-      deleteBtn.classList.add("delete-btn");
-
-      todoList.classList.add("custom-div");
-      todoList.append(li, deleteBtn);
-
-      todosDiv.appendChild(todoList);
-
-      headingDiv.appendChild(todosDiv);
-    });
-    projects.append(headingDiv);
-  });
 };
 
 const showForm = (container, inbox, btn) => {
