f7cf808ab8cd4525fdfd54649adf3d8bb38fbbbb
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-03-07 09:47:07 -0800

    Update index.js with if statements for localStorage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import "./styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import screenController from "./controllers/screenController.js";
 
-console.log(localStorage.length);
+if (!localStorage.length) {
+  console.log("It's empty!");
+} else {
+  console.log("It's not empty!");
+  console.log(localStorage);
+}
 
 screenController.init();
